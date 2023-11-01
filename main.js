f290b8c056273c6e60ba4849d75fb6cce98234be
Rafaela P. da Costa <rafaelapc33@gmail.com>
2023-11-01 20:33:54 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,3 @@
-
-
-// const peso = parseInt(prompt('Informe o seu peso: ', 'Digite aqui'));
-// const altura = parseFloat(prompt('Informe a sua altura: ', 'Digite aqui'));
 function calcImc() {
     const pesoInput = document.getElementById("inputPeso");
     const alturaInput = document.getElementById("inputAlt");
@@ -13,38 +9,17 @@ function calcImc() {
     const resDiv = document.getElementById("resDiv");
 
     if (imc < 18.5) {
-        resDiv.innerHTML = `Abaixo do peso`;
+        resDiv.innerHTML = "Abaixo do peso";
     } else if (imc < 24.9) {
-        resDiv.innerHTML = `Peso normal`;
+        resDiv.innerHTML = "Peso normal";
     } else if (imc < 29.9) {
-        resDiv.innerHTML = `Sobrepeso`;
+        resDiv.innerHTML = "Sobrepeso";
     } else if (imc < 34.9) {
-        resDiv.innerHTML = `Obesidade grau 1`;
+        resDiv.innerHTML = "Obesidade grau 1";
     } else if (imc < 39.9) {
-        resDiv.innerHTML = `Obesidade grau 2`;
+        resDiv.innerHTML = "Obesidade grau 2";
     } else {
-        resDiv.innerHTML = `Obesidade grau 3`;
+        resDiv.innerHTML = "Obesidade grau 3";
     }
 }
 
-
-
-
-// js_n1 = parseInt(window.prompt('Informe o primeiro número: ' , 'Digite aqui',00));
-// js_n2 = parseInt(window.prompt('Informe o segundo número: ' , 'Digite aqui',00));
-
-
-// /* Saída de Dados sem classificação */
-// document.write('Prim. número digitado =' + js_n1 );
-// document.write('<br>Seg. número digitado =' + js_n2 );
-
-// //Saída de Dados com IF...ELSE
-//     if (js_n1 > js_n2){
-//         document.write('<p><b> Classificação em Ordem Crescente:</b>' + js_n1 + 'e' + js_n2);
-//     }
-//     else{
-//     document.write('<p><b> Classificação em Ordem Crescente:</b>' + js_n2 + 'e' + js_n1);
-//     }
-//         -->
-// </script>
-// </body>
